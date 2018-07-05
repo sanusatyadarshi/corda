af9cacc8c92bdd3c5de206e106978dbbc9117233
RogerWillis <roger.willis@r3cev.com>
2018-07-05 16:26:08 +0100

    * Updated javadoc for WaitForStatesToUpdate aysnc operation.

--- core/src/main/kotlin/net/corda/core/flows/AsyncOperations.kt
+++ core/src/main/kotlin/net/corda/core/flows/AsyncOperations.kt
@@ -7,7 +7,16 @@ import net.corda.core.internal.concurrent.asCordaFuture
 import net.corda.core.node.ServiceHub
 import java.util.concurrent.CompletableFuture
 
-/** An [FlowAsyncOperation] which suspends a flow until the provided [StateRef]s have been updated. */
+/**
+ * An [FlowAsyncOperation] which suspends a flow until the provided [StateRef]s have been updated.
+ *
+ * WARNING! Remember that the flow which uses this async operation will _NOT_ wake-up until all the supplied StateRefs
+ * have been updated. If the node isn't aware of the supplied StateRefs or if the StateRefs are never updated, then
+ * the calling flow will remain suspended.
+ *
+ * @property stateRefs the StateRefs which will be updated.
+ * @property services a ServiceHub instance
+ */
 class WaitForStatesToUpdate(val stateRefs: Set<StateRef>, val services: ServiceHub) : FlowAsyncOperation<Unit> {
     override fun execute(): CordaFuture<Unit> {
         val futures = stateRefs.map { stateRef -> services.vaultService.whenConsumed(stateRef).toCompletableFuture() }
